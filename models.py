ed39e11264126aba5c19ba9f6db201dd22ca6a82
oscar <corcasta@outlook.com>
2020-10-05 10:46:11 -0500

    Implementación de Modelos: Critic & Actor

--- models.py
+++ models.py
@@ -8,11 +8,20 @@ class Critic(tf.keras.Model):
         self.hidden_l2 = tf.keras.layers.Dense(600, activation=tf.nn.relu)
         self.output_l = tf.keras.layers.Dense(1, activation=tf.nn.tanh)
 
-    def call(self, inputs, **kwargs):
-        x = self.hidden_l1(inputs)
+    def call(self, input_tensor, **kwargs):
+        x = self.hidden_l1(input_tensor)
         x = self.hidden_l2(x)
         return self.output_l(x)
 
+    def build_graph(self, input_shape, to_file):
+        x = tf.keras.Input(shape=input_shape)
+        model = tf.keras.Model(inputs=x, outputs=self.call(x))
+        return tf.keras.utils.plot_model(model, to_file=to_file, show_shapes=True)
+
+    def print_summary(self, input_shape):
+        x = tf.keras.Input(shape=input_shape)
+        model = tf.keras.Model(inputs=x, outputs=self.call(x))
+        return model.summary()
 
 class Actor(tf.keras.Model):
     def __init__(self, **kwargs):
@@ -21,7 +30,12 @@ class Actor(tf.keras.Model):
         self.hidden_l2 = tf.keras.layers.Dense(600, activation=tf.nn.relu)
         self.output_l = tf.keras.layers.Dense(1, activation=tf.nn.tanh)
 
-    def call(self, inputs, **kwargs):
-        x = self.hidden_l1(inputs)
+    def call(self, input_tensor, **kwargs):
+        x = self.hidden_l1(input_tensor)
         x = self.hidden_l2(x)
         return self.output_l(x)
+
+    def print_graph(self, input_shape, to_file):
+        x = tf.keras.Input(shape=input_shape)
+        model = tf.keras.Model(inputs=x, outputs=self.call(x))
+        return tf.keras.utils.plot_model(model, to_file=to_file, show_shapes=True)
